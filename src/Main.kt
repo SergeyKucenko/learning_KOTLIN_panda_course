60784250c52b3286a6e65fdca3f0f05db1815bc5
Sergey Kucenko <seo-ksn@yandex.ru>
2026-03-05 18:21:23 +0300

    3 day learning

--- src/Main.kt
+++ src/Main.kt
@@ -1,12 +1,7 @@
 fun main() {
 
 
-    val winterTemperature = -27.5 // отрицательное значение температуры
-    val summerTemperature = +26.3 // положительное значение температуры
-
-    val isEmptySpaceship = true // корабль пуст - да
-    val isFullSpaceship = !isEmptySpaceship // корабль полный = корабль не пуст
-
+    println("Hello World!")
 
 }
 
