863a35f8b0da145ae3192126ed1c2bd3715cb97e
Sergey Kucenko <seo-ksn@yandex.ru>
2026-03-06 20:59:36 +0300

    4 day learning is End

--- src/SumunCourse.kt
+++ src/SumunCourse.kt
@@ -1,17 +1,10 @@
 fun main() {
 
-    val name = readln()
-    val date = readln()
-    val cityBorn = readln()
-    val father = readln()
-    val mother = readln()
-    val brother = readln()
-    val wife = readln()
+    val jonAge = readln().toInt()
+    val nikAge = readln().toInt()
+    val sum = jonAge*nikAge
 
-
-
-    println("Имя: $name\nДата рождения: $date\nМесто рождения: $cityBorn\nОтец: $father\nМать: $mother\nБрат: $brother\nЖена: $wife")
-
+    println("Разница в возрасте: $sum лет " )
 
 
 
