c5871f2ae8b7fdf0932ef5c942ac6cf31ce9819a
Sergey Kucenko <seo-ksn@yandex.ru>
2026-03-07 20:13:25 +0300

    5 day(short) learning is  sart i End

--- src/PandaCourse.kt
+++ src/PandaCourse.kt
@@ -1,4 +1,10 @@
 fun main() {
 
+    val productName = readln()
+    val completedProductArea = readln()
+
+    val result = productName == "Нано-помидор" && completedProductArea == "100 Га"
+
+    println(result)
 
 }
