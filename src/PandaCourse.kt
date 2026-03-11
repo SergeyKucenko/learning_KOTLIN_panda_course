b22ce366eba128281d8edb7b4eb9419c99a91112
Sergey Kucenko <seo-ksn@yandex.ru>
2026-03-11 19:16:09 +0300

    9 day learning is End

--- src/PandaCourse.kt
+++ src/PandaCourse.kt
@@ -1,44 +1,93 @@
 fun main() {
 
-    println("Введите статус погоды: ")
-    val weatherStatus = readln()
 
-    println("Введите компонент, требующий ремонта: ")
-    val componentName = readln()
+    println("Введите валюту (прием):")
+    val inputCurrency = readln()
 
-    if (weatherStatus == "Хорошая") {
-        if (componentName == "Колеса") {
-            println("Стоимость ремонта: 150 кредитов")
-        } else if (componentName == "Корпус") {
-            println("Стоимость ремонта: 250 кредитов")
-        } else if (componentName == "Двигатель") {
-            println("Стоимость ремонта: 500 кредитов")
+    println("Введите валюту (получение):")
+    val outputCurrency = readln()
+
+    println("Введите количество валюты на обмен:")
+    val amountOfCurrency = readln().toInt()
+
+    if (inputCurrency == "Кредиты") {
+        if (outputCurrency == "Серебро") {
+            val result = amountOfCurrency * 0.05
+            println("Выдано (серебро): $result")
+        } else if (outputCurrency == "Золото") {
+            val result = amountOfCurrency * 0.05
+            println("Выдано (золото): $result")
         } else {
-            println("Стоимость ремонта: определить не удалось")
+            println("Обмен \"$inputCurrency -> $outputCurrency\" не поддерживается")
+        }
+    } else if (inputCurrency == "Серебро") {
+        if (outputCurrency == "Кредиты") {
+            val result = amountOfCurrency * 0.05
+            println("Выдано (кредиты): $result")
+        } else if (outputCurrency == "Золото") {
+            val result = amountOfCurrency * 0.05
+            println("Выдано (золото): $result")
+        } else {
+            println("Обмен \"$inputCurrency -> $outputCurrency\" не поддерживается")
+        }
+    } else if (inputCurrency == "Золото") {
+        if (outputCurrency == "Серебро") {
+            val result = amountOfCurrency * 0.05
+            println("Выдано (серебро): $result")
+        } else if (outputCurrency == "Кредиты") {
+            val result = amountOfCurrency * 0.05
+            println("Выдано (кредиты): $result")
+        } else {
+            println("Обмен \"$inputCurrency -> $outputCurrency\" не поддерживается")
+        }
+    } else if (inputCurrency == "Драхмы") {
+        if (outputCurrency == "Кредиты") {
+            val result = amountOfCurrency /20.0
+            println("Выдано (кредиты): $result")
+        } else if (outputCurrency == "Драхмы") {
+            val result = amountOfCurrency * 0.05
+            println("Выдано (кредиты): $result")
+        } else {
+            println("Обмен \"$inputCurrency -> $outputCurrency\" не поддерживается")
         }
     }
 
-    if (weatherStatus == "Плохая") {
-        if (componentName == "Колеса") {
-            println("Стоимость ремонта: 300 кредитов")
-        } else if (componentName == "Корпус") {
-            println("Стоимость ремонта: 500 кредитов")
-        } else if (componentName == "Двигатель") {
-            println("Стоимость ремонта: 1000 кредитов")
-        } else {
-            println("Стоимость ремонта: определить не удалось")
-        }
-    }
 
 
-//    if (componentName == "Колеса") {
-//        println("Стоимость ремонта: 150 кредитов")
-//    } else if (componentName == "Корпус") {
-//        println("Стоимость ремонта: 250 кредитов")
-//    } else if (componentName == "Двигатель") {
-//        println("Стоимость ремонта: 500 кредитов")
-//    } else {
-//        println("Стоимость ремонта: определить не удалось")
+
+
+
+//________________________________________________________________________________________________
+//    println("Введите статус погоды: ")
+//    val weatherStatus = readln()
+//
+//    println("Введите компонент, требующий ремонта: ")
+//    val componentName = readln()
+//
+//    if (weatherStatus == "Хорошая") {
+//        if (componentName == "Колеса") {
+//            println("Стоимость ремонта: 150 кредитов")
+//        } else if (componentName == "Корпус") {
+//            println("Стоимость ремонта: 250 кредитов")
+//        } else if (componentName == "Двигатель") {
+//            println("Стоимость ремонта: 500 кредитов")
+//        } else {
+//            println("Стоимость ремонта: определить не удалось")
+//        }
 //    }
+//
+//    if (weatherStatus == "Плохая") {
+//        if (componentName == "Колеса") {
+//            println("Стоимость ремонта: 300 кредитов")
+//        } else if (componentName == "Корпус") {
+//            println("Стоимость ремонта: 500 кредитов")
+//        } else if (componentName == "Двигатель") {
+//            println("Стоимость ремонта: 1000 кредитов")
+//        } else {
+//            println("Стоимость ремонта: определить не удалось")
+//        }
+//    }
+//________________________________________________________________________________________________
+
 
 }
