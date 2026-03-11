70671922fc3b472c5da2756d3c07e6d0b0220552
Sergey Kucenko <seo-ksn@yandex.ru>
2026-03-11 18:25:04 +0300

    9 day learning

--- src/PandaCourse.kt
+++ src/PandaCourse.kt
@@ -1,6 +1,44 @@
 fun main() {
 
+    println("Введите статус погоды: ")
+    val weatherStatus = readln()
+
+    println("Введите компонент, требующий ремонта: ")
+    val componentName = readln()
+
+    if (weatherStatus == "Хорошая") {
+        if (componentName == "Колеса") {
+            println("Стоимость ремонта: 150 кредитов")
+        } else if (componentName == "Корпус") {
+            println("Стоимость ремонта: 250 кредитов")
+        } else if (componentName == "Двигатель") {
+            println("Стоимость ремонта: 500 кредитов")
+        } else {
+            println("Стоимость ремонта: определить не удалось")
+        }
+    }
+
+    if (weatherStatus == "Плохая") {
+        if (componentName == "Колеса") {
+            println("Стоимость ремонта: 300 кредитов")
+        } else if (componentName == "Корпус") {
+            println("Стоимость ремонта: 500 кредитов")
+        } else if (componentName == "Двигатель") {
+            println("Стоимость ремонта: 1000 кредитов")
+        } else {
+            println("Стоимость ремонта: определить не удалось")
+        }
+    }
 
 
+//    if (componentName == "Колеса") {
+//        println("Стоимость ремонта: 150 кредитов")
+//    } else if (componentName == "Корпус") {
+//        println("Стоимость ремонта: 250 кредитов")
+//    } else if (componentName == "Двигатель") {
+//        println("Стоимость ремонта: 500 кредитов")
+//    } else {
+//        println("Стоимость ремонта: определить не удалось")
+//    }
 
 }
