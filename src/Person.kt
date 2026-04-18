830f37753c6f54995cb6eba7082c75d68358cb5e
Sergey Kucenko <seo-ksn@yandex.ru>
2026-04-18 13:15:22 +0300

    46 day learning is Start

--- src/Person.kt
+++ src/Person.kt
@@ -1,31 +1,56 @@
-class Person {
-
-    var name: String = ""
-    var age: Int = 0
-    var weight: Double = 0.0
-    var height: Double = 0.0
-
-    fun sayHello() {
-        println("HELLO")
-    }
-
-    fun run() {
-        repeat(10) {
-            println("Running....")
-        }
-    }
-}
-
-//_______________________________________________________________
-
-//class Car {
+//class Person {
 //
-//    var brand: String = ""
-//    var model: String = ""
-//    var enginePower: Int = 0
-//    var bodyColor: String = ""
+//    var name: String = ""
+//    var age: Int = 0
+//    var weight: Double = 0.0
+//    var height: Double = 0.0
 //
-//    fun drive() {
-//        println("Еду на автомобиле")
+//    fun sayHello() {
+//        println("HELLO")
+//    }
+//
+//    fun run() {
+//        repeat(10) {
+//            println("Running....")
+//        }
 //    }
 //}
+
+
+//class Assistant {
+//    var name: String = ""
+//    var famaly: String = ""
+//
+//
+//    fun bringCoffee(drinkName: String, count: Int) {
+//
+//        repeat(count) {
+//
+//            println("Get UP")
+//            println("Go to the coffee machine")
+//            println("Push button \"$drinkName\"")
+//            println("wait")
+//            println("get your cup")
+//        }
+//    }
+//}
+//_______________________________________________________________
+
+class Car {
+
+    var brand: String = ""
+    var model: String = ""
+    var enginePower: Int = 0
+    var bodyColor: String = ""
+
+    fun drive() {
+        println("Еду на автомобиле")
+    }
+
+    fun refuel(nameAzs:String, oilName:String, litri:Int) {
+        println("Произведена заправка на АЗС \"$nameAzs\" + ")
+        println("Марка бензина: $oilName")
+        println("Кол-во литров: $litri")
+    }
+
+}
