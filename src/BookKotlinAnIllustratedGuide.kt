12ced453e8bff256a54f5038de557614c493d175
Sergey Kucenko <seo-ksn@yandex.ru>
2026-04-20 17:39:34 +0300

    48 day learning is Start

--- src/BookKotlinAnIllustratedGuide.kt
+++ src/BookKotlinAnIllustratedGuide.kt
@@ -4,30 +4,19 @@ fun main() {
 //    ФУНКЦИИ стр 33
 
 
-//    val radius = readln().toDouble();
-//
-//    val nomNumber = circumference(radius)
-//    println("Окружность равна: $nomNumber")
-//
-//
-//}
-//
-//
-//
-//fun circumference(radius: Double): Double {
-//    val pi = 3.14
-//    return 2 * pi * radius
-//}
+    val radius = readln().toDouble();
 
-    var input = readln().toInt();
+    val nomNumber = circumference(radius)
+    println("Окружность равна: $nomNumber")
 
-    val resulte = test(10, age=input);
-    println(resulte)
+
 }
 
-fun test (data:Int = 5, age : Int):Int {
 
-    val resultat = data * age
 
-    return resultat
+fun circumference(radius: Double): Double {
+    val pi = 3.14
+    return 2 * pi * radius
 }
+
+
