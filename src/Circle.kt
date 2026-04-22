db334da6bc4b1445036b4a1c05342892edd41231
Sergey Kucenko <seo-ksn@yandex.ru>
2026-04-22 15:47:19 +0300

    45 day is learning KOTLIN

--- src/Circle.kt
+++ src/Circle.kt
@@ -1,3 +1,21 @@
+//val smallCircle = Circle(5.2)
+//val mediumCircle = Circle(6.7)
+//val largeCircle = Circle(10.0)
+
+val smallCircle = Circle(5.2)
+val radiusOfSmallCircle: Double = smallCircle.radius
+val circumferenceOfSmallCircle = smallCircle.circumference()
+val areaOfSmallCircle = smallCircle.area()
+val diameterOfSmallCircle = smallCircle.diameter()
+
+
 class Circle (var radius: Double) {
+    private val pi: Double = 3.14
+
+    fun circumference() = diameter() * pi
+    fun area() = pi * radius * radius
+    fun diameter() = 2 * radius
+
 
 }
+
