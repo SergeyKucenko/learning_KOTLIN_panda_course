601db31ddb3d353c81f362a927853cb984adb572
Sergey Kucenko <seo-ksn@yandex.ru>
2026-04-23 17:57:27 +0300

    51 day is learning KOTLIN

--- src/BookKotlinAnIllustratedGuide.kt
+++ src/BookKotlinAnIllustratedGuide.kt
@@ -4,19 +4,30 @@ fun main() {
 //    ФУНКЦИИ стр 33
 
 
-    val radius = readln().toDouble();
 
-    val nomNumber = circumference(radius)
-    println("Окружность равна: $nomNumber")
+    val isDark: Boolean = true
+    val isRaining: Boolean = false
+
+    val shouldTurnOnHeadlights = isDark.or(isRaining)
+
+    val shouldStayHome = isDark.and(isRaining)
+
+    println(shouldTurnOnHeadlights)
+    println(shouldStayHome)
+
+//    val radius = readln().toDouble();
+//
+//    val nomNumber = circumference(radius)
+//    println("Окружность равна: $nomNumber")
 
 
 }
 
 
 
-fun circumference(radius: Double): Double {
-    val pi = 3.14
-    return 2 * pi * radius
-}
+//fun circumference(radius: Double): Double {
+//    val pi = 3.14
+//    return 2 * pi * radius
+//}
 
 
