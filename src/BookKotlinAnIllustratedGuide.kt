2c3d7f69c694feaf687e1d143335b51f7a695618
Sergey Kucenko <seo-ksn@yandex.ru>
2026-05-05 19:02:14 +0300

    59 day is learning KOTLIN

--- src/BookKotlinAnIllustratedGuide.kt
+++ src/BookKotlinAnIllustratedGuide.kt
@@ -1,28 +1,6 @@
 fun main() {
 
 
-    val nameOfSchnauzer: String = "Шейди"
-
-    val breedOfSchnauzer: String = "Миниатюрная"
-
-
-
-    fun describe(breed: SchnauzerBreed) = when (breed) {
-        SchnauzerBreed.MINIATURE -> "Маленькая"
-        SchnauzerBreed.STANDARD -> "Средняя"
-        SchnauzerBreed.GIANT -> "Крупная"
-
-
-    }
-
-
-    val breed: SchnauzerBreed = SchnauzerBreed.GIANT
-
-    println(SchnauzerBreed.GIANT.height)
-
-    println(SchnauzerBreed.STANDARD.family)
-    println(SchnauzerBreed.STANDARD.isShorterThan(40))
-
 
 
 }
--- src/Circle.kt
+++ src/Circle.kt
@@ -2,21 +2,7 @@
 import kotlin.math.PI
 
 
-enum class SchnauzerBreed(val height:Int) {
-    MINIATURE(33),
-    STANDARD(47),
-    GIANT(65);
 
-    val family: String = "Schnauzer"
-
-    fun isShorterThan(centimeters: Int) = height < centimeters
-
-
-    fun describe(breed: SchnauzerBreed) {
-        println(breed.name)
-        println(breed.height)
-    }
-}
 
 
 
